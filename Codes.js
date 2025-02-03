f9c3e0c1c90fda355509258ec6347a2ee34c85a8
skvertyy <121826201+ripbomb@users.noreply.github.com>
2025-02-03 18:53:30 +0300

    Update Codes.js

--- Codes.js
+++ Codes.js
@@ -1,12 +1,12 @@
 [
     {
         "code": 123456,
-        "username": 7110075138
-        "ip-adress": 95.24.99.169
+        "username": 7110075138,
+        "ip-address": "95.24.99.160"
     },
     {
         "code": 236462,
-        "username": 7330662936
-        "ip-adress": 95.24.99.168
+        "username": 7330662936,
+        "ip-address": "95.24.99.160"
     }
 ]
